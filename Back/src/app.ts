b4a816f50d71badb15b5586cb3ecadd57ceef1f2
Junior Prado <juniorcvnnn@gmail.com>
2023-03-03 17:20:29 -0300

    modificações 2.0

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const app = express();
 
 app.use(express.json());
 app.use(cors());
+const port=process.env||3003;
 
-app.listen(3003,() => {
-    console.log("Servidor rodando na porta 3003");
+app.listen(port,() => {
+    console.log(`Servidor rodando na porta ${port}` );
     
 })
