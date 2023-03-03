6d090dd63d932959a9b55b6b711a533319e4c40e
Junior Prado <juniorcvnnn@gmail.com>
2023-03-03 17:12:43 -0300

    modificações

--- Back/build/index.js
+++ Back/build/index.js
@@ -4,6 +4,9 @@ const clientRouter_1 = require("./router/clientRouter");
 const app_1 = require("./app");
 const productRouter_1 = require("./router/productRouter");
 const orderRouter_1 = require("./router/orderRouter");
+app_1.app.get("/", (req, resp) => {
+    resp.send("API Mine Markert ok!");
+});
 app_1.app.use("/client", clientRouter_1.clientRouter);
 app_1.app.use("/product", productRouter_1.productRouter);
 app_1.app.use("/order", orderRouter_1.orderRouter);
