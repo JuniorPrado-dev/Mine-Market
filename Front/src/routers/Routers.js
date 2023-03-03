3aece9c68f223d4672f76c1513edb43f0df93cde
Junior Prado <juniorcvnnn@gmail.com>
2023-03-03 15:47:53 -0300

    correção

--- Front/src/routers/Routers.js
+++ Front/src/routers/Routers.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import EndPage from '../pages/EndPage';
+import EndPage from '../pages/endPage';
 import Home from '../pages/Home';
 
 const Routers = () => {
     return <BrowserRouter>
         <Routes>
-            <Route index element={<Home/>} />
+            <Route path='/' element={<Home/>} />
             <Route path='/end-order' element={<EndPage/>} />
         </Routes>
     </BrowserRouter>
