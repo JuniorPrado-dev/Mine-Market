fed1bb986cd0f235e85b2be5a241860bc87bbc2f
Junior Prado <juniorcvnnn@gmail.com>
2023-03-03 14:07:01 -0300

    Back Refatorado

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,39 +1,8 @@
-import { getStock } from './endpoints/stock';
-import { createOrder } from './endpoints/createOrder';
-import { getAllProducts } from './endpoints/allProducts';
-import { getAllClients } from './endpoints/allClients';
-import { createClient } from './endpoints/createClient';
-import { ping } from './endpoints/ping';
-import express from "express";
-import cors from "cors";
-import {AddressInfo} from "net"
-const app = express();
+import { clientRouter } from './router/clientRouter';
+import { app } from "./app";
+import { productRouter } from './router/productRouter';
+import { orderRouter } from './router/orderRouter';
 
-app.use(express.json());
-app.use(cors());
-
-const server=app.listen(process.env.PORT || 3003,()=>{
-    if(server){
-        const adress = server.address() as AddressInfo;
-        console.log(`server is runing in ${adress.port}`);
-        
-    }else{
-        console.error("Faillur uppon start in server! ")
-    }
-});
-
-app.get("/ping",ping );
-
-//retorna dados dos clientes
-app.get("/clients",getAllClients);
-//retorna dados dos produtos
-app.get("/products",getAllProducts);
-//retorna estoque
-app.get("/stock",getStock);
-
-
-
-//retorna dados dos produtos
-app.post("/order",createOrder);
-//add client
-app.post("/client",createClient);
+app.use("/client",clientRouter);
+app.use("/product",productRouter);
+app.use("/order",orderRouter);
--- Back/src/models/Products.ts
+++ Back/src/models/Products.ts
@@ -1,5 +1,30 @@
-export type TProduct={
-    name?:string;
-    id:number;
-    qty:number;
+// export type TProduct={
+//     name?:string;
+//     id:number;
+//     qty:number;
+// }
+
+export class Product{
+    constructor( 
+        public id: string, 
+        public name: string, 
+        public price: number, 
+        public qty_stock:number){} 
+}
+export class ProductGetAllOutputDTO{
+    constructor( 
+        public id: string, 
+        public name: string, 
+        public price: number, 
+        public qtyStock:number){} 
+}
+export class ProductStockDTO{
+    constructor(
+        public name:string,
+        public qty_stock:number){}
+}
+export class ProductStockOutputDTO{
+    constructor(
+        public name:string,
+        public qtyStock:number){}
 }
